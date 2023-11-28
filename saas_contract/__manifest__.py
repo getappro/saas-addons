51f01a461b6e2ba21de7c861e24bde90a65a3425
GetApPRO <81932927+getappro@users.noreply.github.com>
2023-11-29 00:45:42 +0100

    Update __manifest__.py

--- saas_contract/__manifest__.py
+++ saas_contract/__manifest__.py
@@ -7,11 +7,11 @@
     "category": "Sales",
     # "live_test_url": "http://apps.it-projects.info/shop/product/DEMO-URL?version=14.0",
     "images": [],
-    "version": "15.0.1.0.1",
+    "version": "16.0.1.0.1",
     "application": False,
 
     "author": "IT-Projects LLC, Eugene Molotov",
-    "support": "apps@it-projects.info",
+    "support": "a6ps@it-projects.info",
     "website": "https://apps.odoo.com/apps/modules/14.0/saas_contract/",
     "license": "Other OSI approved licence",  # MIT
     # "price": 9.00,
