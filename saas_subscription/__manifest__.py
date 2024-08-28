06c32586740d0eb7f1d49de77e2bc702fc8250bc
GetApPRO <81932927+getappro@users.noreply.github.com>
2024-08-28 02:28:09 +0100

    Create __manifest__.py

---
+++ saas_subscription/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'SaaS Subscription',
+    'summary': 'Module to create a build SaaS from Subscription Module',
+    'version': '16.0.0.0.0',
+    'category': 'Extra Tools',
+    'author':'GetapPRO',
+    'maintainer': 'GetapPRO',
+    'website': 'http://www.getap.pro',
+    'license': 'AGPL-3',
+    'depends': ['subscription_package'],
+    'data': [
+    ],
+}
